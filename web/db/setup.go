2991d42d50a0d126a25228c1e9b4dcd607621989
fauxriarty <aditya.chheda04@gmail.com>
2024-01-21 01:32:21 +0530

    initial commit

---
+++ web/db/setup.go
@@ -0,0 +1,17 @@
+package db
+
+import (
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
+var DB *gorm.DB
+
+func ConnectDatabase() {
+	dsn := "host=db.bpqqtgfwuvoopopmvsfl.supabase.co user=postgres password=Karanrupaakchandramaditya dbname=postgres port=5432"
+	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	if err != nil {
+		panic(err)
+	}
+
+}
